5287f91aeb677721d54cc5f18fe6fcf97290d9af
Victor Nina <viconina10@gmail.com>
2025-05-18 04:00:14 -0400

    Delete routes directory

--- routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { register, login } = require('../controllers/authController');
-console.log('register:', register);
-router.post('/register', register);
-router.post('/login', login);
-
-module.exports = router;
